3a56b6b537fdc21fcd9d3bc9c293cf95beb70a6e
d36choi <yuio1126@gmail.com>
2021-01-17 18:06:56 +0900

    refactor: Refactoring AppConfig

--- src/main/java/shop/core/AppConfig.java
+++ src/main/java/shop/core/AppConfig.java
@@ -15,14 +15,23 @@ public class AppConfig {
      * @return 
      * 여기서 멤버서비스의 구현체를 무엇으로 할지를 결정시키도록 한다. --> 생성자 주입의 원리
      * 외부에서 구현객체의 주입을 결정.
+     * refactor: 역할과 구현을 구분한다. new MemoryMemberRepository() 부분이 중복 제거 되었다
      */
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
-    
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),
+                discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
